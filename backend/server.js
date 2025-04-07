73729a7e1f1aa3d7929a3f1d3f1c5a5cb210bb55
Ailin Ayelen Miño <pukyailin@gmail.com>
2025-04-07 20:03:24 -0300

    generador de saludos

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/saludo/:nombre', (req, res) => {
+    const nombre = req.params.nombre;
+    res.send(`Hola, ${nombre}!`);
+});
+
+app. listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
